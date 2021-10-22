d54ae10bf25dbc4f0a4a603c77cfbd8392578a22
JP <jprshaid123@gmail.com>
2021-10-22 16:17:50 -0300

    Test de Interactuar

---
+++ Assets/0_Scripts/LevelMechanics/Node.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public NodeType nodeType;
+    public bool isConnected;
+    public bool isGrabbed;
+    public NodeConnection nodes;
+
+
+    public void EquipNode()
+    {
+        isGrabbed = true;
+        nodes.equippedNode = this;
+    }
+
+    public void ConnectNode()
+    {
+        if (nodes.equippedNode.nodeType == nodeType)
+        {
+            EventManager.Trigger("ConnectNode", nodeType);
+            Debug.Log("Conecto!");
+            isConnected = true;
+        }
+        else { 
+            Debug.Log("No conecto!");
+            isGrabbed = false;
+        } 
+        nodes.equippedNode = null;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && other.gameObject.tag == "Player")
+        {
+            if (nodes.equippedNode == null)
+            {
+                EquipNode();
+                Debug.Log("GRABBED");
+            } else if (!isConnected && !isGrabbed)
+            {
+                ConnectNode();
+                Debug.Log("CONNECTING");
+            }
+        }
+    }
+}
+
+
+
+public enum NodeType
+{
+    RED,
+    BLUE,
+    GREEN
+}
---
+++ Assets/0_Scripts/LevelMechanics/NodeConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeConnection : MonoBehaviour
+{
+    public List<Node> nodes = new List<Node>();
+    public List<GameObject> cables;
+
+    public Node equippedNode;
+
+    private void Start()
+    {
+        EventManager.Subscribe("ConnectNode", ConnectNode);
+    }
+
+    public void ConnectNode(object[] parameters)
+    {
+        cables[(int)parameters[0]].SetActive(true);
+    }
+    
+}
