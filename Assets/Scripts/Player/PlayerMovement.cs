a7e4857ed6059abb3057ee01f93d4cc836b8dc0b
MaiineV <agustinmartinezbarrios2@hotmail.com>
2021-10-22 10:26:41 -0300

    Movimiento y camara

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Fisicas
+    [SerializeField] private Rigidbody _rb = null;
+
+    //Movement Inputs
+    [SerializeField] private float _speed = 0;
+    private float _horizontal, _vertical;
+    private float _movementMagnitud;
+    private Vector3 _movementVector;
+
+    //VectoresMouse
+    Vector2 mousePos = new Vector2();
+    Vector3 point = new Vector3();
+
+
+    void Update()
+    {
+        Movement();
+        Rotation();
+    }
+
+    private void Movement()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        _movementVector.x = _horizontal;
+        _movementVector.z = _vertical;
+
+        _movementMagnitud = _movementVector.magnitude;
+
+        if (_movementMagnitud > 1)
+        {
+            _rb.MovePosition(transform.position + _movementVector.normalized * (_speed * Time.deltaTime));
+            _movementMagnitud = 1;
+        }
+        else
+            _rb.MovePosition(transform.position + _movementVector * (_speed * Time.deltaTime));
+    }
+
+    private void Rotation()
+    {
+        mousePos.x = Input.mousePosition.x;
+        mousePos.y = Input.mousePosition.y;
+
+        point = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+        point.y = 1;
+
+        transform.LookAt(point);
+    }
+}
