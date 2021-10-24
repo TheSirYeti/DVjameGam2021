ac79abf3df210420b2877eb777d1fccfd656debe
JP <jprshaid123@gmail.com>
2021-10-24 18:10:25 -0300

    El maldito clippy bortherrrrr

---
+++ Assets/0_Scripts/Enemies/Clippy/Clippy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Clippy : MonoBehaviour
+{
+    public List<GameObject> clippyPoints;
+    public float timeToAttack;
+    private bool canAttack = true;
+    public float timeAlive;
+
+    private void Start()
+    {
+        StartCoroutine(AttackCycle());
+    }
+
+    IEnumerator AttackCycle()
+    {
+        while (canAttack)
+        {
+            StartCoroutine(SpawnAttack());
+            yield return new WaitForSeconds(timeToAttack);
+        }
+    }
+
+    IEnumerator SpawnAttack()
+    {
+        int rand = Random.Range(0, clippyPoints.Count);
+        clippyPoints[rand].SetActive(true);
+        yield return new WaitForSeconds(timeAlive);
+        clippyPoints[rand].SetActive(false);
+    }
+}
