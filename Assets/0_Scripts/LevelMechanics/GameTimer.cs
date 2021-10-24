25834ca6415a76cc95e9d85e8a463a9b05ccd1da
JP <jprshaid123@gmail.com>
2021-10-24 18:36:07 -0300

    Added timer

--- Assets/0_Scripts/LevelMechanics/GameTimer.cs
+++ Assets/0_Scripts/LevelMechanics/GameTimer.cs
@@ -1,9 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
-    public float time;
+    public float timer;
+    public Text timeText;
     
+    private void Update()
+    {
+        ClockTimer();
+    }
+
+    public void ClockTimer()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        timeText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+        timer -= Time.fixedDeltaTime;
+    }
 }
