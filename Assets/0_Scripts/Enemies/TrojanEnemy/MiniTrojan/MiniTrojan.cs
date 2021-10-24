40e61960f9cc61cd6ee3938c13dde3e2aac9a371
JP <jprshaid123@gmail.com>
2021-10-24 14:59:37 -0300

    Mini-troyan

---
+++ Assets/0_Scripts/Enemies/TrojanEnemy/MiniTrojan/MiniTrojan.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class MiniTrojan : Enemy
+{
+    public float timeToExplode;
+    public GameObject explosionPrefab;
+    public bool canMove;
+    public Material originalMaterial;
+    public Material tickMaterial;
+    void Start()
+    {
+        originalMaterial = GetComponent<MeshRenderer>().material;
+        StartCoroutine(MakeExplosion());
+    }
+
+    void Update()
+    {
+        if (canMove)
+        {
+            Vector3 desired = target.transform.position - transform.position;
+            desired.Normalize();
+            desired *= speed;
+
+            Vector3 steering = desired - GetVelocity();
+            steering = Vector3.ClampMagnitude(steering, maxForce);
+            
+            ApplyForce(steering);
+            transform.position += GetVelocity() * Time.deltaTime;
+            transform.forward = GetVelocity().normalized;
+        }
+    }
+    
+    public override void Attack()
+    {
+        var explosion = Instantiate(explosionPrefab);
+        explosion.transform.position = transform.position;
+        canMove = false;
+        Destroy(gameObject);
+    }
+
+    public IEnumerator MakeExplosion()
+    {
+        while (canMove)
+        {
+            GetComponent<MeshRenderer>().material = tickMaterial;
+            timeToExplode -= Time.fixedDeltaTime;
+            yield return new WaitForSeconds(timeToExplode / 2);
+            GetComponent<MeshRenderer>().material = originalMaterial;
+            yield return new WaitForSeconds(timeToExplode / 2);
+            if (timeToExplode <= 0.05f)
+            {
+                Attack();
+                StopCoroutine(MakeExplosion());
+            }
+        }
+    }
+}
---
+++ Assets/0_Scripts/Enemies/TrojanEnemy/MiniTrojan/MiniTrojanExplosion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniTrojanExplosion : MonoBehaviour
+{
+    public float explosionTime;
+
+    private void Start()
+    {
+        StartCoroutine(Explode());
+    }
+
+    IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(explosionTime);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //menos vida
+        }
+    }
+}
